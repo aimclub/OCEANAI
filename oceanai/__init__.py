317ac1491f88ee347322e3b60e5b03b58a2f49c4
Dmitry Ryumin <dl_03.03.1991@mail.ru>
2022-12-28 16:56:39 +0300

    Summary

--- oceanai/__init__.py
+++ oceanai/__init__.py
@@ -8,7 +8,7 @@ __summary__ = 'OCEANAI'
 __uri__ = 'https://github.com/DmitryRyumin/oceanai'
 
 __version__ = '1.0'
-__release__ = __version__ + '.0a2'
+__release__ = __version__ + '.0a3'
 
 __author__ru__ = 'Рюмина Елена, Рюмин Дмитрий, Карпов Алексей'
 __author__en__ = 'Elena Ryumina, Dmitry Ryumin, Alexey Karpov'
