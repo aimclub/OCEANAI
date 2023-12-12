7c3befef3ebde419b215a94af2c945a2e1ee56d0
Elena Ryumina <58599671+ElenaRyumina@users.noreply.github.com>
2023-12-12 17:59:30 +0300

    Summary

--- oceanai/modules/lab/keras_vggface/utils.py
+++ oceanai/modules/lab/keras_vggface/utils.py
@@ -8,26 +8,6 @@
 import numpy as np
 from keras import backend as K
 
-V1_LABELS_PATH = "https://github.com/rcmalli/keras-vggface/releases/download/v2.0/rcmalli_vggface_labels_v1.npy"
-V2_LABELS_PATH = "https://github.com/rcmalli/keras-vggface/releases/download/v2.0/rcmalli_vggface_labels_v2.npy"
-
-VGG16_WEIGHTS_PATH = "https://github.com/rcmalli/keras-vggface/releases/download/v2.0/rcmalli_vggface_tf_vgg16.h5"
-VGG16_WEIGHTS_PATH_NO_TOP = (
-    "https://github.com/rcmalli/keras-vggface/releases/download/v2.0/rcmalli_vggface_tf_notop_vgg16.h5"
-)
-
-RESNET50_WEIGHTS_PATH = "https://github.com/rcmalli/keras-vggface/releases/download/v2.0/rcmalli_vggface_tf_resnet50.h5"
-RESNET50_WEIGHTS_PATH_NO_TOP = (
-    "https://github.com/rcmalli/keras-vggface/releases/download/v2.0/rcmalli_vggface_tf_notop_resnet50.h5"
-)
-
-SENET50_WEIGHTS_PATH = "https://github.com/rcmalli/keras-vggface/releases/download/v2.0/rcmalli_vggface_tf_senet50.h5"
-SENET50_WEIGHTS_PATH_NO_TOP = (
-    "https://github.com/rcmalli/keras-vggface/releases/download/v2.0/rcmalli_vggface_tf_notop_senet50.h5"
-)
-
-VGGFACE_DIR = "models/vggface"
-
 
 def preprocess_input(x, data_format=None, version=1):
     x_temp = np.copy(x)
